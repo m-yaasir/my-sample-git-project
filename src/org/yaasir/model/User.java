ec3789fc040462ae21fdd829c1506113a82af47d
Yaasir <MYN>
2021-03-02 12:56:22 +0400

    Added getter and setter for the name field

--- src/org/yaasir/model/User.java
+++ src/org/yaasir/model/User.java
@@ -3,4 +3,12 @@ package org.yaasir.model;
 public class User {
 
 	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
